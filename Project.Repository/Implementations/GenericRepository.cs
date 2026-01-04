515f486acaeafaeedab54bc350d1bd5fb76fe9ac
abdulbasitofficialpk <abdulbasit21s21@gmail.com>
2026-01-05 03:21:53 +0500

    Including this project Repository Class Liberary in side two folder interfaces and implementations. In side interfaces folder make IGenericRepository And the other side under the implementations folder make class GenericRepository.

---
+++ Project.Repository/Implementations/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using Project.Model;
+using Project.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Repository.Implementations
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ProjectEntities _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(ProjectEntities context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.AsNoTracking().ToListAsync();
+        }
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+        public async Task AddAsync(T entity)
+        {
+            _dbSet.Add(entity);
+            await Task.CompletedTask;
+        }
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await Task.CompletedTask;
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if(entity != null)
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+    }
+}
