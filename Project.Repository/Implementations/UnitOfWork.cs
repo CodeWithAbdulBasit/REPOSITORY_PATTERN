e47e1129e2b75a0e71e920d4606c81855898e0e6
abdulbasitofficialpk <abdulbasit21s21@gmail.com>
2026-01-16 03:01:02 +0500

    include UOW Implementation inside reposity class liberary

---
+++ Project.Repository/Implementations/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using Project.Model;
+using Project.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Repository.Implementations
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ProjectEntities _context;
+        private readonly Dictionary<Type, object> _repositories = new Dictionary<Type, object>();
+        public UnitOfWork(ProjectEntities context)
+        {
+            _context = context;
+        }
+        public IGenericRepository<T> Repository<T>() where T : class
+        {
+            if (_repositories.ContainsKey(typeof(T)))
+                return (IGenericRepository<T>)_repositories[typeof(T)];
+
+            var repo = new GenericRepository<T>(_context);
+            _repositories.Add(typeof(T), repo);
+            return repo;
+        }
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
